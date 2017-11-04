3aa0d4f03f54ff5a0f4f42350b9529a51c048ecb
EdMacall <irvedwmac@aol.com>
2017-11-04 12:20:48 -0400

    got seeds.sql working

--- seeds.sql
+++ seeds.sql
@@ -13,16 +13,17 @@ INSERT INTO `yourvey`.`questions` (`id`, `text`, `answerType`, `questionNumber`,
             ('5', 'What electronic devices do you own?', 'CheckBoxes', '5', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '1'),
             ('6', 'Who is your favorite musician?', 'TextBox', '6', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '1');
 
-INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`) VALUES ('1', 'Spaghetti', '0', '1', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '1');
-INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`) VALUES ('2', 'Pizza', '0', '2', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '1');
-INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`) VALUES ('3', 'Burgers', '0', '3', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '1');
-INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`) VALUES ('4', 'Ohio', '0', '1', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '2');
-INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`) VALUES ('5', 'Montana', '0', '2', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '2');
-INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`) VALUES ('6', 'Pennsylvania', '0', '3', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '2');
-INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`) VALUES ('7', 'Indiana', '0', '4', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '2');
-INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`) VALUES ('8', 'cats', '0', '1', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '4');
-INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`) VALUES ('9', 'dogs', '0', '2', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '4');
-INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`) VALUES ('10', 'Phone', '0', '1', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '5');
-INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`) VALUES ('11', 'Laptop Computer', '0', '2', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '5');
-INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`) VALUES ('12', 'Desktop Computer', '0', '3', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '5');
-INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`) VALUES ('13', 'Tablet', '0', '4', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '5');
+INSERT INTO `yourvey`.`answers` (`id`, `text`, `voteCount`, `answerNumber`, `createdAt`, `updatedAt`, `QuestionId`)
+     VALUES ('1', 'Spaghetti', '0', '1', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '1'),
+            ('2', 'Pizza', '0', '2', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '1'),
+            ('3', 'Burgers', '0', '3', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '1'),;
+            ('4', 'Ohio', '0', '1', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '2'),
+            ('5', 'Montana', '0', '2', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '2'),
+            ('6', 'Pennsylvania', '0', '3', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '2'),
+            ('7', 'Indiana', '0', '4', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '2'),
+            ('8', 'cats', '0', '1', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '4'),
+            ('9', 'dogs', '0', '2', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '4'),
+            ('10', 'Phone', '0', '1', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '5'),
+            ('11', 'Laptop Computer', '0', '2', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '5'),
+            ('12', 'Desktop Computer', '0', '3', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '5'),
+            ('13', 'Tablet', '0', '4', '2017-11-01 17:41:11', '2017-11-01 17:41:11', '5');
