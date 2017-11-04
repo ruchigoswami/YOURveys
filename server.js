60f046aba77e4fe4531aefc4e0740bde9a2891dc
Mike S <mjstephenson@me.com>
2017-11-04 13:55:27 -0400

    Fixed the issue with static files not being found.

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // Static directory
-app.use(express.static("app/public"));
+app.use(express.static("public"));
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
